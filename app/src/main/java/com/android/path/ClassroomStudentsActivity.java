1d92f94c47d9290c22c8e076a7491c484581712b
ParthSatija <parth001@e.ntu.edu.sg>
2016-12-10 22:16:14 +0800

    Classroom Student Activity Layout

--- app/src/main/java/com/android/path/ClassroomStudentsActivity.java
+++ app/src/main/java/com/android/path/ClassroomStudentsActivity.java
@@ -2,14 +2,35 @@ package com.android.path;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ClassroomStudentsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boolean classHasStudents = hasStudents();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classroom_students);
-//        setContentView(R.layout.all_students_sample);
+        if(classHasStudents){
+            Button view_students = (Button)findViewById(R.id.btnViewClassroomStudents);
+            view_students.setVisibility(View.VISIBLE);
+            ImageButton add_students = (ImageButton)findViewById(R.id.btnAddStudents);
+            add_students.setVisibility(View.INVISIBLE);
+            TextView classroom_msg = (TextView) findViewById(R.id.addStudentMsg);
+            String message = "We have found "+ getNoOfStudents() + "students in your class";
+            classroom_msg.setText(message);
+        }
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_classroom_students);
 
+    }
+    private boolean hasStudents(){
+        return false;
+    }
+    private int getNoOfStudents(){
+        return 23;
     }
 }
